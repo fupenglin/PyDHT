e0c88a36542c49d34a11f8b198ebc55ccb096e81
linfuepng <linfupeng@gmail.com>
2015-09-10 22:54:49 +0800

    xxx

--- dht_store.py
+++ dht_store.py
@@ -3,6 +3,7 @@
 
 from threading import Thread
 from Queue import Queue
+import urllib2
 
 
 class DHTStore(Thread):
@@ -16,6 +17,14 @@ class DHTStore(Thread):
         while self.is_working:
             info_hash = self.queue.get()
             print info_hash.upper()
+           # http://bt.box.n0808.com/F8/F2/F8181597B51C157FB470E5EE236E364C6FBC2AF2.torrent
+            url = 'http://bt.box.n0808.com/' + info_hash[0:2] + '/' + info_hash[len(info_hash) - 2:] + '/' + info_hash + '.torrent'
+            print url
+            response = urllib2.urlopen(url)
+            with open(info_hash, 'wb') as f:
+                f.write(response.read())
+                f.close()
+            self.is_working = False
 
     def save_info_hash(self, info_hash):
         self.queue.put(info_hash)
@@ -24,3 +33,8 @@ class DHTStore(Thread):
         self.is_working = False
 
 
+
+if __name__ == '__main__':
+    store = DHTStore()
+    store.start()
+    store.save_info_hash('6163B1A152E928BEA4146D354B2F5FCE4E781EB4')
