c6258b1113bf839e0aa1f2f97e5eb3c05268b286
unknown <linfupeng@gmail.com>
2015-09-11 10:43:41 +0800

    xxx

--- dht_store.py
+++ dht_store.py
@@ -1,30 +1,26 @@
 # -*- coding:utf-8 -*-
 
 
-from threading import Thread
-from Queue import Queue
-import urllib2
+import threading
+import Queue
+import sqlite3
 
 
-class DHTStore(Thread):
+class DHTStore(threading.Thread):
 
     def __init__(self):
-        Thread.__init__(self)
-        self.queue = Queue()
+        threading.Thread.__init__(self)
+        self.queue = Queue.Queue()
         self.is_working = True
 
     def run(self):
+        self.db = sqlite3.connect('dht.db')
         while self.is_working:
-            info_hash = self.queue.get()
-            url = 'http://bt.box.n0808.com/' + info_hash[0:2] + '/' + info_hash[len(info_hash) - 2:] + '/' + info_hash + '.torrent'
-            print url
-            try:
-                response = urllib2.urlopen(url)
-                with open(info_hash, 'wb') as f:
-                    f.write(response.read())
-                    f.close()
-            except Exception:
-                pass
+            size = self.queue.qsize()
+            while size > 0:
+                info_hash = self.queue.get()
+                self.db.execute("insert into table_info (info_hash) values (?)", [info_hash])
+            self.db.commit()
 
     def save_info_hash(self, info_hash):
         self.queue.put(info_hash.upper())
@@ -32,9 +28,3 @@ class DHTStore(Thread):
     def stop(self):
         self.is_working = False
 
-
-
-if __name__ == '__main__':
-    store = DHTStore()
-    store.start()
-    store.save_info_hash('6163B1A152E928BEA4146D354B2F5FCE4E781EB4')
