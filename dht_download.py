ca8b42c13ff5d1a46664e3023a3015f9df1f9649
linfuepng <linfupeng@gmail.com>
2015-09-18 00:34:50 +0800

    xxx

--- dht_download.py
+++ dht_download.py
@@ -11,15 +11,16 @@ class DHTDownload:
     def __init__(self):
         self.conn = MySQLdb.connect(host='123.56.126.47', user='root', passwd='root', db='dht', port=3306, charset='UTF8')
         self.cur = self.conn.cursor()
+        self.cur.execute("set names utf8")
 
     def test(self):
-        self.cur.execute('select * from dht_info')
+        self.cur.execute('select * from dht_info limit 18')
         for info in self.cur.fetchall():
             try:
                 if info[1]:
                     t = info[1].decode('utf-8')
-                    continue
-                info_hash = '94F3A52B5012AFA8B1D866C0A248EE1FCCA0B522'
+                   # continue
+                info_hash = u'94E62A80A46EB05F01A6D6142FAE585F1F588455'
                 url = "http://bt.box.n0808.com/%s/%s/%s.torrent" % (info_hash[:2], info_hash[38:], info_hash)
                 torrent = urllib2.urlopen(url, timeout=10)
                 raw_data = torrent.read()
@@ -33,7 +34,7 @@ class DHTDownload:
         ok, msg = dht_bencode.decode(content)
         if not ok:
             return
-        info = msg['info']['name']
+        info = msg['info']['name'].decode('utf-8')
         sql = "update dht_info set dht_info.detail_info='%s' where dht_info.info_hash='%s'" % (info, info_hash)
         self.cur.execute(sql)
 
